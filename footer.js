764d09edfdd3428f40e8f945ce8e1d143e2c6b95
Od4ir <od4ir@usp.br>
2023-04-25 13:28:35 -0300

    Criação do footer e adição de alguns arquivos de teste

---
+++ footer.js
@@ -0,0 +1,10 @@
+document.write('\
+\
+    <footer>\
+        <div class="div_footer_img"><img id="img_footer_1" class="img_footer" src="Imagens/Imagens_gerais/logo_usp_1500.png" alt=""></div>\
+        <div class="div_footer_img"><img id="img_footer_2" class="img_footer" src="Imagens/Imagens_gerais/logo_ime_500.png" alt=""></div>\
+        <div class="div_footer_img"><img id="img_footer_3" class="img_footer" src="Imagens/Imagens_gerais/logo_icpc.png" alt=""></div>\
+        <p>2023 @Alumni Maratona de Programação</p>\
+    </footer>\
+\
+');
